e2bcd0e960b6ef6e896ec3bac3d960ee09d627fb
abdo3212 <42039102+abdo3212@users.noreply.github.com>
2018-08-29 12:45:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '+'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`by abdo99 +help`,"http://twitch.tv/S-F")
+client.user.setGame(`by abdo99 +help`,"http://twitch.tv")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
