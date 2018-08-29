b2798b08615ec47a06181cbf858257eb0db62f8c
abdo3212 <42039102+abdo3212@users.noreply.github.com>
2018-08-29 12:11:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '+'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Alpha Codes `,"http://twitch.tv/Alpha Codes")
+client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
