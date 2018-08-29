c9c783e08ae2c87097c589b1ef615ad6154e178c
abdo3212 <42039102+abdo3212@users.noreply.github.com>
2018-08-29 13:00:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -146,7 +146,31 @@ client.on('message' , message => {
 });
 
 
+client.on('message', message => {
+var prefix = "+";
+      if(message.content === prefix + "hide-ch") {
+      if(!message.channel.guild) return;
+      if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply('You Dont Have Perms ❌');
+             message.channel.overwritePermissions(message.guild.id, {
+             READ_MESSAGES: false
+ })
+              message.channel.send('Channel Hided Successfully ! ✅  ')
+ }
+});
+	  
 
+	  
+client.on('message', message => {
+var prefix = "+";
+      if(message.content === prefix + "unhide-ch") {
+      if(!message.channel.guild) return;
+      if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply('❌');
+             message.channel.overwritePermissions(message.guild.id, {
+             READ_MESSAGES: true
+ })
+              message.channel.send('Done  ')
+ }
+});	
 
 
 
