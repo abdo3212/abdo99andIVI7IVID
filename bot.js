20f11e0b01d6e336f5d412d6469e09844138cbf0
abdo3212 <42039102+abdo3212@users.noreply.github.com>
2018-08-30 17:40:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2199,7 +2199,48 @@ message.author.sendMessage(embed);
 
 
 
+   client.on("message", async message => {
+var prefix = "+";
+var aoasm =[
+    {q:"سؤال ",a:"الجواب"},
+    {q:"سؤال ",a:"الجواب"},
+    {q:"سؤال ",a:"الجواب"},
+    
+   ];
+    if(message.content == prefix+"lol"){
+        if(UserBlocked.has(message.guild.id)) return message.channel.send("هناك جلسة .")
+        UserBlocked.add(message.guild.id)
+        var ask = aoasm[Math.floor(Math.random() * aoasm.length)];
+        let embed = new Discord.RichEmbed()
+        .setTitle('LOL')
+        .setAuthor(message.author.username, message.author.avatarURL)
+        .setColor("RANDOM")
+        .setDescription(ask.q);
+        message.channel.sendEmbed(embed).then(msg=> msg.delete(20000))
+        const msgs = await message.channel.awaitMessages(msg => msg.author.id !== client.user.id ,{maxMatches:1,time:10000});
+            UserBlocked.delete(message.guild.id)
+        msgs.forEach(result => {
+           if(result.author.id == client.user.id) return;
+           if(result.content == "") return
+           if(result.content == ask.a){
+             let embeds = new Discord.RichEmbed()
+             .setTitle(':white_check_mark: اجابة صحيحة')
+             .setAuthor(message.author.username, message.author.avatarURL)
+             .setColor("RANDOM")
+             .setDescription(`**${result.author.username}** الإجابة صحيحة`);
+                message.channel.sendEmbed(embeds);                return;
+           } else {
 
+                                  var embedx = new Discord.RichEmbed()
+                .setTitle(':x:خطاء')
+                .setAuthor(message.author.username, message.author.avatarURL)
+                .setColor("RANDOM")
+                .setDescription(`**${result.author.username}** الإجابة خاطئة`);
+                message.channel.sendEmbed(embedx);
+           }
+     });
+  }
+});
 
 
 
